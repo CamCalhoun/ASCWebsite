cb860dccac768dbeb90d6e159bcaf30ac9d63a05
Cameron Calhoun <cameron.calhoun01@gmail.com>
2025-01-20 21:03:17 -0500

    fixed spelling error

--- articles.js
+++ articles.js
@@ -3,7 +3,7 @@ const articles = {
   <h1 class="font-bold text-[#0F172A] text-3xl">About ASC</h1>
   <br>
   <h2 class="font-bold text-[#0F172A] text-2xl">What is ASC?</h2>
-    <p>AI Study Companion (ASC) is an in progress, AI powered study companion hosted on the web. ASC can tracks the users progress in selected subjects, and pushes them in the correct direction to facilitate learning.</p>
+    <p>AI Study Companion (ASC) is an in progress, AI powered study companion hosted on the web. ASC can track the users progress in selected subjects, and pushes them in the correct direction to facilitate learning.</p>
   <br>
   <h2 class="font-bold text-[#0F172A] text-2xl">Why choose ASC?</h2>
     <p>ASC differs from other study companions with its built in AI capabilities. ASC will not use AI to verify a students work, but to procedurally generate questions and study materials tailored to each individaul user. The more you interact with ASC, the better ASC will grow to understand your strengths and weaknesses regarding various subjects.</p>

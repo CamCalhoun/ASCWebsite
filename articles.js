7cac469820e435123c0a2a41b0de5631bdabf713
Cameron Calhoun <cameron.calhoun01@gmail.com>
2025-01-22 17:31:38 -0500

    ver 2.0, added week 1 contents, added tailwindcss/typographies, adjusted stylings

--- articles.js
+++ articles.js
@@ -1,17 +1,13 @@
 const articles = {
-  about: `
-  <h1 class="font-bold text-[#0F172A] text-3xl">About ASC</h1>
-  <br>
-  <h2 class="font-bold text-[#0F172A] text-2xl">What is ASC?</h2>
+    about: `
+  <h1>About ASC</h1>
+  <h2>What is ASC?</h2>
     <p>AI Study Companion (ASC) is an in progress, AI powered study companion hosted on the web. ASC can track the users progress in selected subjects, and pushes them in the correct direction to facilitate learning.</p>
-  <br>
-  <h2 class="font-bold text-[#0F172A] text-2xl">Why choose ASC?</h2>
+  <h2>Why choose ASC?</h2>
     <p>ASC differs from other study companions with its built in AI capabilities. ASC will not use AI to verify a students work, but to procedurally generate questions and study materials tailored to each individaul user. The more you interact with ASC, the better ASC will grow to understand your strengths and weaknesses regarding various subjects.</p>
-  <br>
-  <h2 class="font-bold text-[#0F172A] text-2xl">What technologies does ASC utilize?</h2>
+  <h2>What technologies does ASC utilize?</h2>
     <p>ASC will be a full-stack web application with a React front-end and a Python back-end utilizing the FastAPI library.</p>
-  <br>
-  <h2 class="font-bold text-[#0F172A] text-2xl">Who created ASC?</h2>
+  <h2>Who created ASC?</h2>
     <p>ASC is a student created senior project for Pennsylvania Western University driven by the following members:</p>
     <ul>
       <li>Cameron Calhoun</li>
@@ -21,24 +17,53 @@ const articles = {
     </ul>
     <p>All members are students of Pennsylvania Western University studying Computer Science.</p>
     `,
-  week1: `
-  <h2 class="font-bold text-[#0F172A]">Week 1</h2>
-    <p>Week 1's content will be updated at a future date with this weeks progress report and presentation.</p>`,
+    week1: `
+  <h1>Week 1</h1>
+  <h2>Summary:</h2>
+    <p>This first week was spent setting up our development environment, developing the project website, and making the Gantt chart. Overall, everything went according to plan. The only change made is that we will no longer be using Docker. Due to compatibility challenges we reevaluated our need for Docker and realized that we would be able to work fine without it.</p>
+  <h2>What was Accomplished:</h2>
+    <ul>
+      <li>Gantt Chart</li>
+      <li>Git Repository</li>
+      <li>FastAPI and React communcation</li>
+      <li>Website for weekly reports</li>
+    </ul>
+  <h2>Problems Encountered:</h2>
+    <ul>
+      <li>Docker compatability issues</li>
+    </ul>
+  <h2>Plans to Overcome Problems:</h2>
+    <ul>
+      <li>No longer using Docker. We determined that we wouldn't be losing annything by not using it.</li>
+    </ul>
+  <h2>Plans for next week:</h2>
+    <ul>
+      <li>Get API keys</li>
+      <li>Begin backend development</li>
+    </ul>
+  <h2>Member Contributions:</h2>
+    <ul>
+      <li>Cameron Calhoun - Website</li>
+      <li>Jonathan Buckel - Weekly Report</li>
+      <li>Gage Keslar - Gantt Chart</li>
+      <li>Seth Morgan - Presentation</li>
+    </ul>
+  `
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  const sidebarItems = document.querySelectorAll('[data-section]')
-  const mainArticle = document.getElementById('main-article')
+    const sidebarItems = document.querySelectorAll('[data-section]')
+    const mainArticle = document.getElementById('main-article')
 
-  mainArticle.innerHTML = articles['about']
+    mainArticle.innerHTML = articles['about']
 
-  sidebarItems.forEach(item => {
-    item.addEventListener('click', () => {
-      const section = item.getAttribute('data-section')
-      if (articles[section]) {
-        mainArticle.innerHTML = articles[section]
-      }
+    sidebarItems.forEach(item => {
+        item.addEventListener('click', () => {
+            const section = item.getAttribute('data-section')
+            if (articles[section]) {
+                mainArticle.innerHTML = articles[section]
+            }
+        })
     })
-  })
 
 })
